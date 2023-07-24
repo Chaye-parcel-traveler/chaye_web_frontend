1dc153f8f1ec571bcd6dee9c1dbfef5a5822b1e1
TALEB Meryem <112691864+Meryemtaleb@users.noreply.github.com>
2023-07-24 21:52:18 +0200

    edit styles navbar and home

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,57 +1,57 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Dropdown } from 'semantic-ui-react';
 import '../styles/nav.css';
 
-const Navbar = () => {
-  const [activeItem, setActiveItem] = useState('accueil');
-
-  const handleItemClick = (name) => {
-    setActiveItem(name);
-  };
-
+function Navbar (){
   return (
-    <div className="navbar col-2">
+    <div className="content-menu">
+      <a href="/"> 
+        <img src={"img/logo.png"} alt="Logo" className="logo" />
+      </a>
       <ul className='text-decoration-none'>
-        <a href="/"> <img src={"img/logo.png"} alt="Logo" className="logo" /> </a>
-        <li><a href="/announcements"><i className="  fa-solid fa-bullhorn"></i> Annonces
-        </a></li>
+        <li>
+          <a href="/announcements">
+            <i className="fa-solid fa-bullhorn"></i>
+            <span> Annonces</span>
+          </a>
+        </li>
 
-        <li> <i className="fa-solid me-3 fa-user "></i><Dropdown item text="Mon Compte" className="custom-dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item
-              as="a"
-              href="/SignUp"
-              className={activeItem === 'S\'inscrire' ? 'active' : ''}
-              onClick={() => handleItemClick('SignUp')}
-            >
-              S'inscrire
-            </Dropdown.Item>
-            <Dropdown.Item
-              as="a"
-              href="/login"
-              className={activeItem === 'Se connecter' ? 'active' : ''}
-              onClick={() => handleItemClick('login')}
-            >
-              Se connecter
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown></li>
+        <li> 
+          <i className="fa-solid me-3 fa-user "></i>
+          <Dropdown item text="Mon Compte" className="custom-dropdown">
+              <Dropdown.Menu>
+                <Dropdown.Item  as="a" href="/SignUp"> S'inscrire</Dropdown.Item>
+                <Dropdown.Item as="a" href="/login"> Se connecter</Dropdown.Item>
+              </Dropdown.Menu>
+          </Dropdown>
+        </li>
+
         <li>
-          <a href="/AboutUs"
-          ><i className="fa-solid fa-bullhorn"></i> À propos de nous
-          </a></li>
+            <a href="/AboutUs">
+              <i className="fa-solid fa-bullhorn"></i>
+              <span>À propos de nous</span> 
+          </a>
+        </li>
+
         <li>
-          <a href="/support"><i className="fa-solid fa-circle-info"></i>Support</a></li>
-        <li>  <a
-          href="/allmembers"
-          className={activeItem === 'Membres' ? 'active' : ''}
-          onClick={() => handleItemClick('allmembres')}
-        ><i class="fa-solid fa-users  "></i>
-          AllMembres
-        </a></li>
-        <li>  <a href="http://localhost:5000/logout" >
-          Déconnecter
-        </a></li>
+          <a href="/support">
+            <i className="fa-solid fa-circle-info"></i>
+              <span>Support</span>
+          </a>
+        </li>
+
+        <li>  
+          <a href="/allmembers">
+            <i class="fa-solid fa-users"></i>
+            <span> AllMembres </span> 
+          </a>
+        </li>
+        
+        <li>  
+          <a href="http://localhost:5000/logout" >
+            Déconnecter
+          </a>
+        </li>
       </ul>
     </div>
   );
