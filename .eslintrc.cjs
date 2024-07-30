f56ae8b812320f3e39265be48cde3f2208fc8e7c
jonC0D3R <joncoder.kamite@gmail.com>
2024-07-30 19:45:46 +0200

    adding auto format configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,32 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  plugins: ['react-refresh', "react",
+    "@typescript-eslint",
+    "prettier"],
   rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  }
 }
