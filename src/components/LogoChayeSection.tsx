1ed08419ff49115cea9d5c76868b40d91536128e
jonC0D3R <joncoder.kamite@gmail.com>
2024-10-25 01:50:04 +0200

    adding a:link to the logo

--- src/components/LogoChayeSection.tsx
+++ src/components/LogoChayeSection.tsx
@@ -2,7 +2,9 @@ const LogoChayeSection = () => {
   return (
     <section className="logoChaye">
       <div>
-        <img src="images/logoChaye.png" style={{ display: 'block' }} />
+        <a href="/">
+          <img src="images/logoChaye.png" style={{ display: 'block' }} />
+        </a>
       </div>
     </section>
   );
