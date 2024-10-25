3399e895f00cac98e48aadf8ed606d34327d86f7
jonC0D3R <joncoder.kamite@gmail.com>
2024-10-25 18:13:12 +0200

    add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
 import Navigation from './components/Navigation';
+import SenderFormular from './components/SendFormular/SenderFormular';
+import CarrierFormular from './components/CarrierFormular';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/carrier" element={<CarrierFormular />} />
+          <Route path="/sender" element={<SenderFormular />} />
           {/* <Route path="/profil" element={<ProfilManager />} /> */}
         </Routes>
       </Router>
